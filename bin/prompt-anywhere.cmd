c876b5fd563bd483e41d9a4171820a20437935d3
Steve Dubois <steve.dubois@gent.be>
2023-06-21 09:26:54 +0200

    versie 1.2 opkuis

--- bin/prompt-anywhere.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-set NODE_NO_WARNINGS=1
-set KENV=%~dp0..
-set KNODE=C:\Users\duboisst\.knode
-set KIT=C:\Users\duboisst\.kit
-set TARGET_PATH=C:\Users\duboisst\.kenv\kenvs\cict-scripts\scripts\prompt-anywhere.js
-
-%KNODE%\bin\node.exe %KIT%\run\terminal.js %TARGET_PATH% %*
--- bin/prompt-updates.md
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-NODE_NO_WARNINGS=1 \
-KENV=$(cd "$(dirname ${BASH_SOURCE[0]})"/.. &> /dev/null && pwd) \
-/Users/stevedubois/.knode/bin/node \
-/Users/stevedubois/.kit/run/terminal.js \
-/Users/stevedubois/.kenv/kenvs/cict-scripts/scripts/prompt-updates.md \
-"$@"
